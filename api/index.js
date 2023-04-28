f10cee2a0e5036258ff9592556796b2222fb8462
Suhayb Sattar <suhaybsattar@Suhaybs-MBP.attlocal.net>
2023-04-28 01:02:31 -0500

    changed mongo connection

--- api/index.js
+++ api/index.js
@@ -8,7 +8,17 @@ const cors = require("cors");
 //   console.log(`listening on port ${PORT}`);
 //   app.listen(PORT);
 // });
-mongoose.connect(process.env.MONGO_URL);
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 const { getDeckControllers } = require("./controllers/getDeckControllers");
 const { createDeckController } = require("./controllers/createDeckController");
